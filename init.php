1533f38252011c8ba4f3e1d6e39c67c36d3413a6
Thibac10 <62197827+Thibac10@users.noreply.github.com>
2023-03-25 11:27:12 -0300

    Update init.php

--- init.php
+++ init.php
@@ -2,13 +2,13 @@
  
 // constantes com as credenciais de acesso ao banco MySQL
 define('DB_HOST', 'localhost');
-define('DB_USER', 'filial347');
-define('DB_PASS', 'senhafilial');
-define('DB_NAME', 'atacadao');
+define('DB_USER', 'USER');
+define('DB_PASS', 'PASSWORD');
+define('DB_NAME', 'DB');
  
 // habilita todas as exibições de erros
-ini_set('display_errors', true);
-error_reporting(E_ALL);
+//ini_set('display_errors', true);
+//error_reporting(E_ALL);
  
 // inclui o arquivo de funçõees
 require_once 'functions.php';
